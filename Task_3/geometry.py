947b87b6075418b26d2fa96f6b107dd6689dfa15
Mohamed Magdy <eng.mmagdyhassan@gmail.com>
2026-04-19 21:09:35 +0200

    Delete Task_3 directory

--- Task_3/geometry.py
+++
@@ -1,9 +0,0 @@
-
-
-def circle_area(r):
-    area = 3.14*r*r
-    return area
-
-def square_perimeter(d):
-    perimeter = 4*d
-    return perimeter
--- Task_3/problem4.py
+++
@@ -1,5 +0,0 @@
-
-import geometry
-
-print(geometry.circle_area(2))
-print(geometry.square_perimeter(2))
